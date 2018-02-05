e0d8092eaae29c5685742737f76acfcff1a74d03
zhanghuimeng <zhanghuimeng1997@gmail.com>
2018-02-05 23:23:11 +0800

    Added a popup with settings and login info.
    Now, only the login info is real, but the settings selection is not connected to any real data.

---
+++ popup_login.js
@@ -0,0 +1,20 @@
+if (!chrome.cookies) {
+    chrome.cookies = chrome.experimental.cookies;
+}
+
+function getCookies(domain, name)
+{
+    chrome.cookies.get({"url": domain, "name": name}, function(cookie) {
+        if (cookie != null) {
+            console.log("get cookies: " + cookie.value);
+            document.getElementById("unlogged_info").style.display = "none";
+        }
+        else {
+            document.getElementById("logged_info").style.display = "none";
+        }
+    });
+}
+
+window.onload = function () {
+    getCookies("http://wikidot.com", "WIKIDOT_SESSION_ID");
+};
