00ea194061dc6d759fe94c9f721f8ef905926197
zhanghuimeng <zhanghuimeng1997@gmail.com>
2018-02-06 22:13:52 +0800

    目前在本地可以进行测试了，虽然会挂掉

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'unit_test/*.js'
+      'unit_test/*.js',
+      '*.js'
     ],
 
 
